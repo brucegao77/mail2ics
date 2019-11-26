bb33d69b81ef340b36bc66f637eb6e7ba20444fe
brucegao77 <brucegxs@gmail.com>
2019-11-26 16:48:30 +0800

    feat: Movie release calendar(not finished)

--- task/douban.go
+++ task/douban.go
@@ -7,25 +7,20 @@ import (
 	"log"
 	"net/http"
 	"regexp"
+	"strings"
 )
-
-type list struct {
-	id   int
-	want int
-}
 
 type Movie struct {
 	Id       string `json:"id"`
 	Name     string `json:"name"`
 	Director string `json:"director"`
-	Writer   string `json:"writer"`
+	Author   string `json:"writer"`
 	Casts    string `json:"casts"`
 	Cate     string `json:"cate"`
 	Country  string `json:"country"`
 	Language string `json:"language"`
 	Release  string `json:"release"`
 	Length   string `json:"length"`
-	Alias    string `json:"alias"`
 	Summary  string `json:"summary"`
 	Want     string `json:"want"`
 }
@@ -36,26 +31,26 @@ const (
 )
 
 func MovieSchedule() {
-	ch := make(chan Movie, 1)
-
 	resp, err := getHttpResponser(URL, REFERER)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	ch := make(chan Movie, 1)
 	go parseMovieList(resp, &ch)
 
 	done := make(chan Movie, 1)
 	go func() {
 		for m := range ch {
-			if err = parseMoviePages(&m, &done); err != nil {
+			if err = parseMoviePages(&m); err != nil {
 				log.Fatal(err)
 			}
+			//done <- m
+			break
 		}
 
 		close(done)
 	}()
-
 }
 
 func getHttpResponser(url, referer string) ([]byte, error) {
@@ -88,23 +83,101 @@ func getHttpResponser(url, referer string) ([]byte, error) {
 
 func parseMovieList(resp []byte, ch *chan Movie) {
 	r, _ := regexp.Compile(
-		`<a href="https://movie.douban.com/subject/([0-9]+)/"[\s\S]*?<td class="">[\s\S]*?([0-9]+)人[\s\S]*?</td>`)
+		`<a href="https://movie.douban.com/subject/([0-9]+)/".*?>(.*?)</a>[\s\S]*?` + // Movie id number
+			`<td class="">[\s\S]*?([0-9]+)人[\s\S]*?</td>`) // Want watch
 	for _, id := range r.FindAllStringSubmatch(string(resp), -1) {
 		var m Movie
 		m.Id = id[1]
-		m.Want = id[2]
+		m.Name = id[2]
+		m.Want = id[3]
 		*ch <- m
 	}
 
 	close(*ch)
 }
 
-func parseMoviePages(m *Movie, done *chan Movie) error {
+func parseMoviePages(m *Movie) error {
 	page, err := getHttpResponser(
 		fmt.Sprintf("https://movie.douban.com/subject/%s/", m.Id), URL)
 	if err != nil {
 		return err
 	}
 
+	r, _ := regexp.Compile(
+		`<div id="info">([\s\S]*?)</div>` + // infos
+			`[\s\S]*?` +
+			`<div class="indent" id="link-report">([\s\S]*?)</div>`) // summary
+	items := r.FindStringSubmatch(string(page))
+	infoStr := items[1] // Part of informations
+
+	getSummary(&items[2], &m.Summary)
+	getInfoA(infoStr, "导演", `<a href.*?>(.*?)</a>`, &m.Director)
+	getInfoA(infoStr, "编剧", `<a href.*?>(.*?)</a>`, &m.Author)
+	getInfoA(infoStr, "主演", `<a href.*?>(.*?)</a>`, &m.Casts)
+	getInfoB(infoStr, "类型:", `<span.*?>(.*?)</span>`, &m.Cate)
+	getInfoC(infoStr, "上映日期:", `(\d{4}-\d{2}-\d{2})\(中国大陆\)`, &m.Release)
+	getInfoC(infoStr, "片长:", `>(\d+)分钟`, &m.Length)
+	getInfoD(infoStr, "制片国家/地区:", `(.*?)`, &m.Country)
+	getInfoD(infoStr, "语言:", `(.*?)`, &m.Language)
+	//log.Println(m.Summary)
+
 	return nil
+}
+
+func getSummary(str, filed *string) {
+	var content string
+
+	r, _ := regexp.Compile(`<span property="v:summary" class="">([\s\S]*?)</span>`)
+	if items := r.FindStringSubmatch(*str); len(items) != 0 {
+		content = items[1]
+	} else {
+		r, _ := regexp.Compile(`<span class="all hidden">([\s\S]*?)</span>`)
+		content = r.FindStringSubmatch(*str)[1]
+	}
+
+	strings.Replace(content, " ", "", -1)
+	strings.Replace(content, "<br />", "", -1)
+	*filed = content
+}
+
+// 导演、编剧、主演
+func getInfoA(infoStr, name, re string, filed *string) {
+	r, _ := regexp.Compile(fmt.Sprintf(`<span class='pl'>%s</span>([\s\S]*?)<br/>`, name))
+	filedStr := r.FindStringSubmatch(infoStr)[1]
+
+	r, _ = regexp.Compile(re)
+	for n, i := range r.FindAllStringSubmatch(filedStr, -1) {
+		*filed += i[1] + "  "
+		if n == 2 {
+			break
+		}
+	}
+}
+
+// 类型
+func getInfoB(infoStr, name, re string, filed *string) {
+	r, _ := regexp.Compile(fmt.Sprintf(`<span class="pl">%s</span>([\s\S]*?)<br/>`, name))
+	filedStr := r.FindStringSubmatch(infoStr)[1]
+
+	r, _ = regexp.Compile(re)
+	for _, i := range r.FindAllStringSubmatch(filedStr, -1) {
+		*filed += i[1] + "  "
+	}
+}
+
+// 上映日期、片长
+func getInfoC(infoStr, name, re string, filed *string) {
+	r, _ := regexp.Compile(fmt.Sprintf(`<span class="pl">%s</span>([\s\S]*?)<br/>`, name))
+	filedStr := r.FindStringSubmatch(infoStr)[1]
+
+	r, _ = regexp.Compile(re)
+	if items := r.FindStringSubmatch(filedStr); len(items) != 0 {
+		*filed = items[1]
+	}
+}
+
+// 制片国家、语言
+func getInfoD(infoStr, name, re string, filed *string) {
+	r, _ := regexp.Compile(fmt.Sprintf(`<span class="pl">%s</span>([\s\S]*?)<br/>`, name))
+	*filed = r.FindStringSubmatch(infoStr)[1]
 }
