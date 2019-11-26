6d63e0134ec11bee8c9e4d207ef1319a4488c555
brucegao77 <SCRSgxs000077>
2019-11-26 08:34:32 +0800

    feat: Movie release calendar(not finished)

--- task/douban.go
+++ task/douban.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"regexp"
 )
@@ -14,7 +15,7 @@ type list struct {
 }
 
 type Movie struct {
-	Id       int    `json:"id"`
+	Id       string `json:"id"`
 	Name     string `json:"name"`
 	Director string `json:"director"`
 	Writer   string `json:"writer"`
@@ -29,7 +30,35 @@ type Movie struct {
 	Want     string `json:"want"`
 }
 
-func GetHttpResponser(url, referer string) ([]byte, error) {
+const (
+	URL     = "https://movie.douban.com/coming"
+	REFERER = "https://movie.douban.com/cinema/later/chengdu/"
+)
+
+func MovieSchedule() {
+	ch := make(chan Movie, 1)
+
+	resp, err := getHttpResponser(URL, REFERER)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go parseMovieList(resp, &ch)
+
+	done := make(chan Movie, 1)
+	go func() {
+		for m := range ch {
+			if err = parseMoviePages(&m, &done); err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		close(done)
+	}()
+
+}
+
+func getHttpResponser(url, referer string) ([]byte, error) {
 	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
@@ -57,11 +86,25 @@ func GetHttpResponser(url, referer string) ([]byte, error) {
 	}
 }
 
-func ParseComingList(resp []byte) {
-	fmt.Println(string(resp))
+func parseMovieList(resp []byte, ch *chan Movie) {
 	r, _ := regexp.Compile(
 		`<a href="https://movie.douban.com/subject/([0-9]+)/"[\s\S]*?<td class="">[\s\S]*?([0-9]+)人[\s\S]*?</td>`)
 	for _, id := range r.FindAllStringSubmatch(string(resp), -1) {
-		fmt.Println(id)
+		var m Movie
+		m.Id = id[1]
+		m.Want = id[2]
+		*ch <- m
 	}
+
+	close(*ch)
+}
+
+func parseMoviePages(m *Movie, done *chan Movie) error {
+	page, err := getHttpResponser(
+		fmt.Sprintf("https://movie.douban.com/subject/%s/", m.Id), URL)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
